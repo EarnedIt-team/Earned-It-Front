a969dd030321fa39bba3cff3a13295fc922b7db9
ParkJunYoung <j77777y@naver.com>
2025-08-12 15:02:11 +0900

    [Design] : login - 로그인 View에서 하단 영역 Color가 다른것을 수정 #45

--- lib/views/loading_overlay_view.dart
+++ lib/views/loading_overlay_view.dart
@@ -2,12 +2,8 @@ import 'package:flutter/material.dart';
 
 // 로딩 오버레이
 Widget overlayView() {
-  return Positioned.fill(
-    child: Container(
-      color: Colors.black.withValues(alpha: 0.5), // 불투명도 50% 검은색 배경
-      child: const Center(
-        child: CircularProgressIndicator(color: Colors.white),
-      ),
-    ),
+  return Container(
+    color: Colors.black.withValues(alpha: 0.5), // 불투명도 50% 검은색 배경
+    child: const Center(child: CircularProgressIndicator(color: Colors.white)),
   );
 }
